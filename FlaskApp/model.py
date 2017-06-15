df6c867dcf9ab92df35a7afc391c5a0388af71f3
GhostKU <dev@ghostku.com>
2017-06-15 15:32:32 +0300

    Clean code according to pycharm advises

--- FlaskApp/model.py
+++ FlaskApp/model.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 
-class Person():
+class Person:
     db = None
 
     def __init__(self, config):
@@ -10,21 +10,26 @@ class Person():
             Person.db = sqlite3.connect(self.config.DB_FILE)
 
     def add(self, name, comment):
-        self.db.execute('insert into %s (name, comment) values ("%s", "%s")' % (self.config.TABLE, name, comment))
+        self.db.execute('insert into persons (name, comment) values ("%s", "%s")' % (name, comment))
         self.db.commit()
 
     def read_all(self):
-        res = self.db.execute('select name, comment, id from %s' % (self.config.TABLE,)).fetchall()
+        res = self.db.execute('select name, comment, id from persons').fetchall()
         return [{'id': i[2], 'name': i[0], 'comment': i[1]} for i in res]
 
     def read_random(self, number):
         # Не самый эффективный способ но все же
-        res = self.db.execute('select * from %s order by random() limit %i' % (self.config.TABLE, number)).fetchall()
+        res = self.db.execute('select * from persons order by random() limit %i' % (number, )).fetchall()
         return [{'id': i[0], 'name': i[1], 'comment': i[2]} for i in res]
 
+    def update(self, id_):
+        pass
 
-    def remove(self, id):
+    def read(self, id_):
+        pass
+
+    def remove(self, id_):
         # TODO Добавить проверку существует ли запись
 
-        self.db.execute('delete from %s where id = %s' % (self.config.TABLE, id))
+        self.db.execute('delete from persons where id = %s' % (id_,))
         self.db.commit()
--- FlaskApp/view.py
+++ FlaskApp/view.py
@@ -5,19 +5,23 @@ from flask import render_template, Response, request
 import json
 from werkzeug.exceptions import BadRequest
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/persons/list')
 def get_persons():
     return Response(json.dumps(Person(Config()).read_all()), mimetype='application/json')
 
+
 @app.route('/persons/delete')
 def remove_person():
     id_ = request.args.get('id')
     Person(Config()).remove(id_)
-    return 'Removed succesfully'
+    return 'Removed successfully'
+
 
 @app.route('/persons/add', methods=['post'])
 def add_person():
@@ -26,7 +30,8 @@ def add_person():
     if not name:
         raise BadRequest('Person was not added. Name can not be empty')
     Person(Config()).add(name, comment)
-    return 'Added succesfully'
+    return 'Added successfully'
+
 
 @app.route('/get_winners')
 def get_winners():
